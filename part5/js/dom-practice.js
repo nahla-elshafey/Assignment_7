324e7c5e3072c7b84a2a3de73c540163281ed7fc
nahla-elshafey <elshafey.nahla@gmail.com>
2023-10-23 00:22:03 -0700

    Partial Part 5

--- part5/js/dom-practice.js
+++ part5/js/dom-practice.js
@@ -1,10 +1,24 @@
 /*eslint-env browser*/
 
+/*
 //STEP 1
+function displayMessage() {
+    "use strict";
+    window.alert("I have been clicked (Step 1)");
+}
+*/
 
 //STEP 2
+window.document.getElementById("myButton2").onclick = function () {
+    window.alert("I have been clicked (Step 2)");
+};
 
+/*
 //STEP 3
+window.document.getElementById("myButton").addEventListener("click", function() {
+    window.alert("I have been clicked (Method 3)");
+});
+*/
 
 //STEP 4
 
